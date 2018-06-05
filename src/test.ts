69a121583a0134c5c72d2e8a31bfc3e679cc3e64
AndyGura <andy.gura.jd@gmail.com>
2018-06-05 13:12:16 +0300

    improved testing solving

--- src/test.ts
+++ src/test.ts
@@ -9,29 +9,59 @@ function testSolver(): boolean {
         "higb5a43ffd173ieh2cebfd87i1ba8cg5fd9i3f4h2a7e4ge1if3bh5b49achfg7hie642aca63h2gied",
         "8abgecfdiid36hbagef7ed9a2hca5dbc7hifcfih457babhg1fie3deb1igdc68dc85bfi1gg9fcah4eb"
     ];
+    console.log('Checking solve result.........');
     for (let i: number = 0; i < testCases.length; i++) {
         let sudoku: Sudoku = new Sudoku();
         sudoku.deserialize(testCases[i]);
         try {
             sudoku.solve();
         } catch (err) {
+            console.log('FAIL');
             return false;
         }
         if (sudoku.serialize() != testCases[i]) {
+            console.log('WRONG RESULT');
             return false;
         }
     }
+    console.log('OK');
 
-    console.log('Measuring solve time...');
-    let theMostComplicatedSudoku: string = "8abgecfdiid36hbagef7ed9a2hca5dbc7hifcfih457babhg1fie3deb1igdc68dc85bfi1gg9fcah4eb";
-    let startTime = (new Date()).getTime();
-    let sudoku: Sudoku;
-    for (let i:number = 0; i < 500; i++) {
-        sudoku = new Sudoku();
+    const theMostComplicatedSudoku: string = "8abgecfdiid36hbagef7ed9a2hca5dbc7hifcfih457babhg1fie3deb1igdc68dc85bfi1gg9fcah4eb";
+    const sudokus: Sudoku[] = [];
+    const passes: number = 100;
+
+    let deserializeTime: number;
+    let solveTime: number;
+    let clearTime: number;
+
+    console.log('Profiling.....');
+
+    let startTime: number = Date.now();
+    let now: number;
+    for (let i:number = 0; i < passes; i++) {
+        let sudoku: Sudoku = new Sudoku();
         sudoku.deserialize(theMostComplicatedSudoku);
-        sudoku.solve();
+        sudokus.push(sudoku);
     }
-    console.log('The most complicated sudoku solve time: ' + ((new Date()).getTime() - startTime)/500 + 'ms; complexity: ' + sudoku.complexity);
+    now = Date.now();
+    deserializeTime = now - startTime;
+    startTime = now;
+    for (let i:number = 0; i < passes; i++) {
+        sudokus[i].solve();
+    }
+    now = Date.now();
+    solveTime = now - startTime;
+    startTime = now;
+    for (let i:number = 0; i < passes; i++) {
+        sudokus[i].clear(false);
+    }
+    clearTime = Date.now() - startTime;
+
+    console.log(`The most complicated sudoku time (complexity = ${sudokus[0].complexity}):`);
+    console.log(`\tdeserialize time: ${deserializeTime/passes} ms`);
+    console.log(`\tsolve time: ${solveTime/passes} ms`);
+    console.log(`\tclear time: ${clearTime/passes} ms`);
+
     return true;
 }
 
